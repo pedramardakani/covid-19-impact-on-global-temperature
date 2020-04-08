7be8bcd6e5be5129307ab36c3d32c59fccb7f3b8
Pedram Ashofteh Ardakani <pedramardakani@gmail.com>
2020-04-08 17:38:04 +0430

    Plot skin temperature
    from the data.nc file. Make sure you check the data dimensions. Trying
    to save the data in a text file in a future commit to get my head around
    how this data structures work.

--- ex-03.py
+++ ex-03.py
@@ -5,6 +5,7 @@ Contour Fill on Maps, Pedram.
 import numpy as np
 import Ngl as ngl
 import Nio as nio
+from datetime import date, timedelta
 
 #-- open file and read variables 
 
@@ -12,11 +13,6 @@ f   = nio.open_file('data.nc','r')
 var = f.variables['skt'][0,:,:]
 lat = f.variables['latitude'][:]
 lon = f.variables['longitude'][:]
-
-#-- start the graphics
-
-#wks = ngl.open_wks('pdf','ss_skt_plt')
-wks = ngl.open_wks('png','ss_skt_plt')
 
 #-- resource settings
 
@@ -32,64 +28,87 @@ res.lbOrientation   = "horizontal"
 res.sfXArray    = lon
 res.sfYArray    = lat
 
-### Set the plot name
+print('var shape:',np.shape(f.variables['skt']))
 
-plotname = f.variables['skt'].attributes['long_name'] + ' by Pedram, for Dear Saeed'
+animate = False
+frames = 1
 
-### Check the data type
+for i in range(frames):
+	### start the graphics
+	wks = ngl.open_wks('png','ss_skt_plt')
 
-if(var.dtype != 'float64'):
-	print('\nCaution! Current data type:','(',var.dtype,')\n> Changing to: ( float64 )')
-	var = var.astype('float64')
-	if(var.dtype == 'float64'):
-		print('Success! Data type:',var.dtype)
-		
-### Check if there are scale factors involved
+	### converting time, hours since 1900-01-01
+	
+	hrs	= f.variables['time'][0]
+	print(hrs)
+	tstart	= date(1900,1,1)
+	tdelta	= timedelta(hours=float(hrs))
+	t_date	= tstart + tdelta
+	print('Date:', t_date)
+	
+	### Check the data type
 
-print('\nChecking for scale factors ...')
-sf = f.variables['skt'].attributes['scale_factor']
-if(sf != None):
-	print('> scale factor:',sf)
-	print('> scale factor type:', type(sf))
-	var = var * np.array(sf)
+	if(var.dtype != 'float64'):
+		print('\nCaution! Current data type:','(',var.dtype, \
+			')\n> Changing to: ( float64 )')
+		var = var.astype('float64')
+		if(var.dtype == 'float64'):
+			print('Success! Data type:',var.dtype)
+			
+	### Check if there are scale factors involved
 
-### Check if there are offset values available
+	print('\nChecking for scale factors ...')
+	sf = f.variables['skt'].attributes['scale_factor']
+	if(sf != None):
+		print('> scale factor:',sf)
+		print('> scale factor type:', type(sf))
+		var = var * np.array(sf)
 
-print('\nChecking for offset values ...')
-os = f.variables['skt'].attributes['add_offset']
-if(os != None)
+	### Check if there are offset values available
 
-print('> offset type:',type(os))
-print('> offset value:',os)
+	print('\nChecking for offset values ...')
+	os = f.variables['skt'].attributes['add_offset']
+	if(os != None):
+		print('> offset type:',type(os))
+		print('> offset value:',os)
 
-print(' ################### debugging ################### ')
-print('\nChecking available variables:\n')
-#print('# variable\t: attributes')
-for vt in f.variables:
-	for at in f.variables[vt].attributes:
-		print('> f.variables[',vt,'].attributes[',at,']',sep='')
-	print('')
-print('Done!\n')
-print(f.variables['skt'].attributes['long_name'])
-print(f.variables['skt'].attributes['units'])
-print(type(wks),type(var),type(res))
-print(f.variables['skt'])
-print(' ################### done ################### ')
-#"""
-#-- create the contour plot
-plot = ngl.contour_map(wks,var,res)
+	
+	print(' ################### debugging ################### ')
+	print('\nChecking available variables:\n')
+	#print('# variable\t: attributes')
+	for vt in f.variables:
+		for at in f.variables[vt].attributes:
+			print('> f.variables[',vt,'].attributes[',at,']',sep='')
+		print('')
+	print('Done!\n')
+	print(f.variables['skt'].attributes['long_name'])
+	print(f.variables['skt'].attributes['units'])
+	print(type(wks),type(var),type(res))
+	print(f.variables['skt'])
+	print(f.variables['time'])
+	print(' ################### done ################### ')
+	
+	### Set the plot name
 
-#-- write variable long name and units to the plot
-txres   = ngl.Resources()
-txres.txFontHeightF = 0.012
+	plotname = f.variables['skt'].attributes['long_name'] + \
+		' by Pedram, for Dear Saeed.'
+	
+	#-- create the contour plot
+	plot = ngl.contour_map(wks,var,res)
 
-ngl.text_ndc(wks, plotname, \
-        0.30,0.82,txres)
-ngl.text_ndc(wks, 'Degrees Celsius', \
-        0.90,0.82,txres) # Hard coded this because of inconsistency in data structure: f.variables['skt'].attributes['units']
+	#-- write variable long name and units to the plot
+	txres   = ngl.Resources()
+	txres.txFontHeightF = 0.012
 
-#-- advance the frame
-ngl.frame(wks)
+	ngl.text_ndc(wks, plotname + \
+						'   In degrees Celsius.' + \
+						'   Data date: ' + str(t_date) , \
+						0.50,0.82,txres)
+			# Hard coded this because of inconsistency \
+			# in data structure: f.variables['skt'].attributes['units']
 
-ngl.end()
-#"""
+	#-- advance the frame
+	
+	ngl.frame(wks)
+
+	ngl.end()
