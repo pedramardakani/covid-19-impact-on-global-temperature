fe5847b71adc022cd7889c2d9599332a6e980f83
Pedram Ashofteh Ardakani <pedramardakani@gmail.com>
2020-04-08 22:38:23 +0430

    Extract the data & do the brute-force time check
    to save the data to a text file. Because sometimes we need the data as
    a txt file for some programs that are NOT able to read the nc extension.
    
    The line that saves the actual data is commented out in order to check
    the performance using the `brute-force` method. This method uses the
    difference between time-stamps before and after the process we need to
    time. Then, the result is simply printed on the page.
    
    I've done this in order to compare the performance using the `np.array`
    methods instead of using `for` loops. The methods `repeat` and `tile`
    were the ones I was looking for. I don't know if I'll try reaching the
    same result using `for` loops or other stuff. I just assume that using
    the native `numpy` functions have better performance than `for` loops.

--- ex-03.py
+++ ex-03.py
@@ -8,7 +8,7 @@ Contour Fill on Maps, Pedram.
 import numpy as np
 import Ngl as ngl
 import Nio as nio
-from datetime import date, timedelta
+import datetime
 
 def pedplot():
 	#-- create the contour plot
@@ -30,14 +30,12 @@ def pedplot():
 	ngl.frame(wks)
 
 #-- open file and read variables 
-
 f   = nio.open_file('data.nc','r')
 var = f.variables['skt'][0,:,:]
 lat = f.variables['latitude'][:]
 lon = f.variables['longitude'][:]
 
 #-- resource settings
-
 res = ngl.Resources()
 res.nglFrame    = False
 
@@ -50,8 +48,6 @@ res.lbOrientation   = "horizontal"
 res.sfXArray    = lon
 res.sfYArray    = lat
 
-print('var shape:',np.shape(f.variables['skt']))
-
 animate = False
 frames = 1
 
@@ -60,16 +56,14 @@ for i in range(frames):
 	wks = ngl.open_wks('png','ss_skt_plt')
 
 	### converting time, hours since 1900-01-01
-	
 	hrs	= f.variables['time'][0]
 	print(hrs)
-	tstart	= date(1900,1,1)
-	tdelta	= timedelta(hours=float(hrs))
+	tstart	= datetime.date(1900,1,1)
+	tdelta	= datetime.timedelta(hours=float(hrs))
 	t_date	= tstart + tdelta
 	print('Date:', t_date)
 	
 	### Check the data type
-
 	if(var.dtype != 'float64'):
 		print('\nCaution! Current data type:','(',var.dtype, \
 			')\n> Changing to: ( float64 )')
@@ -78,7 +72,6 @@ for i in range(frames):
 			print('Success! Data type:',var.dtype)
 			
 	### Check if there are scale factors involved
-
 	print('\nChecking for scale factors ...')
 	sf = f.variables['skt'].attributes['scale_factor']
 	if(sf != None):
@@ -87,35 +80,38 @@ for i in range(frames):
 		var = var * np.array(sf)
 
 	### Check if there are offset values available
-
 	print('\nChecking for offset values ...')
 	os = f.variables['skt'].attributes['add_offset']
 	if(os != None):
 		print('> offset type:',type(os))
 		print('> offset value:',os)
-
 	
-	print(' ################### debugging ################### ')
 	print('\nChecking available variables:\n')
-	#print('# variable\t: attributes')
 	for vt in f.variables:
 		for at in f.variables[vt].attributes:
 			print('> f.variables[',vt,'].attributes[',at,']',sep='')
 		print('')
 	print('Done!\n')
-	print(f.variables['skt'].attributes['long_name'])
-	print(f.variables['skt'].attributes['units'])
-	print(type(wks),type(var),type(res))
-	print(f.variables['skt'])
-	print(f.variables['time'])
-	print(' ################### done ################### ')
 	
 	### Set the plot name
-
 	plotname = f.variables['skt'].attributes['long_name'] + \
 		' by Pedram, for Dear Saeed.'
 	
+	start_time = datetime.datetime.now()
 	
+	# == save skin temperature in a separate text file == #
+	nlat = len(f.variables['latitude'])
+	lat = f.variables['latitude'][:]
+	nlon = len(f.variables['longitude'])
+	lon = f.variables['longitude'][:]
+	t = np.ravel(f.variables['skt'][0])
+	x = np.repeat(lat,nlon,axis=0) # rlon
+	y = np.tile(lon,nlat)
+	res = np.vstack((x,y,t))
+	res = np.transpose(res)
+	end_time = datetime.datetime.now()
+	print('time =',end_time - start_time)
+	#np.savetxt("skin-temp.txt", res, fmt=['%5.2f','%3.2f','%5d'], delimiter = " ")
 	
 	# == comment or uncomment next line to get the map outputs == #
 	#pedplot()
