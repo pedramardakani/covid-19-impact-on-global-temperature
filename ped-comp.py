53dbe236d2a457f2bf0212e047a9c4bef075cc7d
Pedram Ashofteh Ardakani <pedramardakani@pm.me>
2020-05-22 21:00:01 +0430

    Change data recall process and reorder commands
    Added comment in the code to make it clear. Long story short, user
    should be aware of the data-set dimensions.

--- ped-comp.py
+++ ped-comp.py
@@ -22,15 +22,38 @@ def h2d(hrs):
 # === set variables  === #
 
 #-- set filename to read the data from
-filename = "data-comp-2.nc"
+filename = "data-comp.nc"
 plotname = "test"
 plottype = "png"
-db = False # set debugging mode on or off
+db = True # set debugging mode on or off
 
 # === start calculations  === #
 
 #-- open file and dicover variables
 f   = nio.open_file(filename,'r')
+
+#-- Check the variable shapes. Prevent contour plot from complaining
+# about the shape. This happened because of the difference between
+# different data structures in the `x.nc` data files. For example,
+# one data-set may contain only data worth of one month, another
+# one may contain several months, and another one may contain data
+# worth of several years! This will introduce different layers of
+# data. Hence, the inconsistency between dimensions.
+#-- The programmer is advised to have a good grasp of the data-sets,
+# and use a method accordingly. The `if` function below will help
+# with better understanding the data-set shape for valid data
+# recollection and usage.
+if db:
+    print('** skin temperature details:')
+    print('############################')
+    print(f.variables['skt'])
+    print('############################')
+    print(np.shape(f.variables['skt']))
+    print('** filename:',filename)
+    print('** available variables:')
+    for vt in f.variables:
+        for at in f.variables[vt].attributes:
+            print("f.variables['",vt,"'].attributes['",at,"']",sep='')
 
 #-- check how many datasets are available under different times
 print(np.size(f.variables['time'].attributes['calendar']))
@@ -40,27 +63,24 @@ for i in f.variables['time']:
     print('i =',i,'Date:',h2d(i))
 
 #-- converting time, hours since 1900-01-01
-t_i = 0 # time index, from zero to
+# time index, from zero to
+t_i = 0
+# this will count how many `time` tables are available:
+t_f = np.shape(f.variables['skt'])[0]
 hrs = f.variables['time'][t_i]
 t_date = h2d(hrs)
 plotname = plotname + str(t_date)
 
-if db:
-    print('** filename:',filename)
-    print('** available variables:')
-    for vt in f.variables:
-        for at in f.variables[vt].attributes:
-            print("f.variables['",vt,"'].attributes['",at,"']",sep='')
-
-
-var = f.variables['skt'][t_i,0,:,:]
+#-- Knowing the dimensions, now we can recall useful data:
+var = f.variables['skt'][t_i,0,0,:,:]
 lat = f.variables['latitude'][:]
 lon = f.variables['longitude'][:]
 
+#-- Recheck the variables shapes in debug mode:
 if db:
-    print('** var size:', np.size(var))
-    print('** lat size:', np.size(lat))
-    print('** lon size:', np.size(lon))
+    print('** var shape:', np.shape(var))
+    print('** lat shape:', np.shape(lat))
+    print('** lon shape:', np.shape(lon))
 
 #-- resource settings
 res = ngl.Resources()
