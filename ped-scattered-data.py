a62dfbcb5064bf38dbd45f2d42b2bb868a87e226
Pedram Ashofteh Ardakani <pedramardakani@pm.me>
2020-06-22 13:30:05 +0430

    Write smart plot program for several datasets
    * check if there are more than two arguments, if so, it is a differnce
      plot, act accordingly

---
+++ ped-scattered-data.py
@@ -0,0 +1,200 @@
+"""
+Contour Fill on Maps, Pedram.
+"""
+
+#-- define a tic, toc function to check performance
+#-- no output? check if the last line that contains the plot function is commented. uncomment for output.
+
+import numpy as np
+import Ngl as ngl
+import Nio as nio
+import datetime
+import sys
+
+# === define functions === #
+
+#-- convert time, hours since 1900-01-01
+def h2d(hrs):
+    tstart = datetime.date(1900,1,1)
+    tdelta = datetime.timedelta(hours=float(hrs))
+    res = tstart + tdelta
+    return res
+
+#-- get time straight from data table
+def gettime(index):
+    hrs = f.variables['time'][index]
+    res = h2d(hrs)
+    return res
+
+#-- get data according to index
+def getdata(index,check_index=0):
+    # check which file to read
+    if index < 18:
+        f   = nio.open_file(filename,'r')
+        # get the data ready
+        var = f.variables['skt'][index,0,check_index,:,:]
+    else:
+        f   = nio.open_file(filename_new,'r')
+        # get the data ready
+        var = f.variables['skt'][18 - index,check_index,:,:]
+
+    #-- Check the data type, if it's anything other than float64, numpy will complain
+    if(var.dtype != 'float64'):
+        if db: print('** caution! current data type:','(',var.dtype,'), changing ...')
+        var = var.astype('float64')
+        if db and (var.dtype == 'float64'):
+            print('** success! new data type:',var.dtype)
+
+    #-- Check if there are scale factors involved
+    sf = f.variables['skt'].attributes['scale_factor']
+    if(sf != None):
+        if db:
+            print('** scale factor detected:',sf)
+            print('** scale factor type:', type(sf))
+        var = var * np.array(sf)
+    return var
+
+# === set variables  === #
+
+check_index = int(sys.argv[1])
+#t_f = 12 # time index, from zero to
+#t_i = 9 # difference
+t_f = int(sys.argv[2])
+#-- check if there are any data to take the difference of
+try:
+    t_i = int(sys.argv[3])
+except:
+    t_i = None
+
+if t_i == None:
+    scalemax = 50
+    scalemin = -1 * scalemax
+    scalestep = 2
+else:
+    scalemax = 20
+    scalemin = -1 * scalemax
+    scalestep = 1
+
+#-- set filename to read the data from
+#-- the data is in absolute form, change it to relative if need be
+filename = "/media/pedram/dataStorage/Ped_Doc/Research/myProjects/saeed-shojaei/pyngl/adaptor.mars.internal-1590162992.3450093-30030-25-7410f2f4-da91-4ef7-bf96-c68cda090a23.nc"
+filename_new =  "/media/pedram/dataStorage/Ped_Doc/Research/myProjects/saeed-shojaei/pyngl/adaptor.mars.internal-1590157005.8169813-22157-7-4452e7e0-a81b-433d-ad07-25b18e498e86.nc"
+
+#-- set saved file name
+plotname = "manual-scale-scattered-data-index-"+str(check_index)+"-"
+#plottype = "png"
+plottype = "pdf"
+db = True # set debugging mode on or off
+
+# === start calculations  === #
+
+#-- open file and dicover variables
+f   = nio.open_file(filename,'r')
+
+#-- check how many datasets are available under different times
+print(np.size(f.variables['time'].attributes['calendar']))
+print(f.variables['time'].attributes['calendar'])
+print(f.variables['time'])
+for i in f.variables['time']:
+    print('i =',i,'Date:',h2d(i))
+
+#-- converting time, hours since 1900-01-01
+#-- check if there is a time difference available
+if t_i == None:
+    plotname = plotname + str(gettime(t_f)) # + " vs " + str(gettime(t_i))
+else:
+    plotname = plotname + str(gettime(t_f)) + "vs" + str(gettime(t_i))
+
+if db:
+    print('** filename:',filename)
+    print('** available variables:')
+    for vt in f.variables:
+        for at in f.variables[vt].attributes:
+            print("f.variables['",vt,"'].attributes['",at,"']",sep='')
+
+
+if db: print('Variable dimensions:', np.shape(f.variables['skt']))
+
+if t_i == None:
+    var = getdata(t_f,check_index)
+else:
+    var = getdata(t_f,check_index) - getdata(t_i,check_index)
+lat = f.variables['latitude'][:]
+lon = f.variables['longitude'][:]
+
+if db:
+    print('** var size:', np.size(var))
+    print('** lat size:', np.size(lat))
+    print('** lon size:', np.size(lon))
+
+#-- resource settings
+res = ngl.Resources()
+# res.nglDraw           = False #-- don't draw the plot yet
+res.nglFrame        = False #-- don't advance the frame yet
+res.cnFillOn        = True  #-- turn on color fill
+if t_i == None:
+
+    res.cnFillPalette   = "cmp_b2r"    #-- set the colormap to be used
+else:
+    res.cnFillPalette   = "temp_diff_18lev"    #-- set the colormap to be used
+
+#res.cnFillPalette   = "ncl_default" #-- set the colormap to be used
+#res.cnFillPalette   = "NCL-BYR-03" #-- set the colormap to be used
+res.cnFillMode      = "RasterFill"
+res.cnLineLabelsOn  = False         #-- turn off contour line labels
+res.cnLinesOn       = False         #-- turn off contour lines
+res.cnInfoLabelOn   = False         #-- turn off contour info labels
+
+res.cnLevelSelectionMode = "ManualLevels" #-- select manual levels
+res.cnMinLevelValF  = scalemin #-- minimum contour value
+res.cnMaxLevelValF  = scalemax #-- maximum contour value
+res.cnLevelSpacingF = scalestep #-- contour increment
+
+res.mpGridAndLimbOn = False
+
+res.lbBoxLinesOn    = False
+res.lbLabelStride   = 10    #-- skip every other label
+#res.pmLabelBarOrthogonalPosF = -0.26 #-- move label upward (how much?)
+res.lbLabelFontHeightF  = 0.009     #-- label bar font height
+res.lbBoxMinorExtentF   = 0.24      #-- decrease height of labelbar box
+res.lbOrientation   = "horizontal"  #-- horizontal labelbar
+
+# res.tiMainString  = "Colors: temperature, Lines: ... "    #-- title string
+
+res.sfXArray        = lon
+res.sfYArray        = lat
+wks = ngl.open_wks(plottype,plotname)
+
+#-- Check the variable before passing it to contour map plot
+if db: print('*** variable type:', type(var),' and shape: ',np.shape(var))
+
+#-- Check if there are offset values available
+os = f.variables['skt'].attributes['add_offset']
+if(os != None):
+    if db:
+        print('** offset value detected:',os)
+        print('** offset value:',type(os))
+    # === what should we do with the offset? === #
+
+#-- Set the plot name
+plotname = f.variables['skt'].attributes['long_name'] + ' -PS- index ' + str(check_index) + '-'
+#-- check if the data is in a difference or not
+
+
+#-- create the contour plot
+plot = ngl.contour_map(wks,var,res)
+
+#-- write variable long name and units to the plot
+txres   = ngl.Resources()
+txres.txFontHeightF = 0.012
+
+#-- check if the data is in a difference or not
+if t_i == None:
+    ngl.text_ndc(wks, plotname + '(C). Date: ' + str(gettime(t_f)), 0.50,0.82,txres)
+else:
+    ngl.text_ndc(wks, plotname + '(C). Date: ' + str(gettime(t_f)) + " vs " + str(gettime(t_i)), 0.50,0.82,txres)
+
+# Hard coded this because of inconsistency in data structure: f.variables['skt'].attributes['units']
+ngl.frame(wks)
+
+ngl.end()
