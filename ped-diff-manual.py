edc095e7882487f7af4242e363c1f31158109dc7
Pedram Ashofteh Ardakani <pedramardakani@pm.me>
2020-07-19 12:30:26 +0430

    Add sysargv for faster automatic envokation

--- ped-diff-manual.py
+++ ped-diff-manual.py
@@ -9,6 +9,7 @@ import numpy as np
 import Ngl as ngl
 import Nio as nio
 import datetime
+import sys
 
 # === define functions === #
 
@@ -26,16 +27,21 @@ def gettime(index):
     return res
 # === set variables  === #
 
-check_index = 9
+check_index = int(sys.argv[1])
+#t_f = 12 # time index, from zero to
+#t_i = 9 # difference
+t_f = int(sys.argv[2])
+t_i = int(sys.argv[3])
 
 #-- set filename to read the data from
 # filename = "adaptor.mars.internal-1587995993.0181034-27757-13-25eca862-11d8-4ecf-97d5-80fe4f17c987.nc"
 filename = "/media/pedram/dataStorage/Ped_Doc/Research/myProjects/saeed-shojaei/pyngl/"\
 			+"adaptor.mars.internal-1590162992.3450093-30030-25-7410f2f4-da91-4ef7-bf96-c68cda090a23.nc"
-			
-plotname = "validity-check-new-data-pack-index-"+str(check_index)+"-"
-plottype = "png"
-db = True # set debugging mode on or off
+
+plotname = "manual-scale-diff-index-"+str(check_index)+"-"
+#plottype = "png"
+plottype = "pdf"
+db = False # set debugging mode on or off
 
 # === start calculations  === #
 
@@ -50,8 +56,6 @@ for i in f.variables['time']:
     print('i =',i,'Date:',h2d(i))
 
 #-- converting time, hours since 1900-01-01
-t_f = 17 # time index, from zero to
-t_i = 9 # difference
 plotname = plotname + str(gettime(t_f)) + " vs " + str(gettime(t_i))
 
 if db:
@@ -75,11 +79,35 @@ if db:
 
 #-- resource settings
 res = ngl.Resources()
-res.nglFrame        = False
-res.cnFillOn        = True
-res.cnFillPalette   = "NCL_default"
-res.cnLineLabelsOn  = False
-res.lbOrientation   = "horizontal"
+# res.nglDraw			= False	#-- don't draw the plot yet
+res.nglFrame        = False	#-- don't advance the frame yet
+res.cnFillOn        = True	#-- turn on color fill
+#res.cnFillPalette   = "cmp_b2r"	#-- set the colormap to be used
+#res.cnFillPalette   = "ncl_default"	#-- set the colormap to be used
+res.cnFillPalette   = "temp_diff_18lev"	#-- set the colormap to be used
+#res.cnFillPalette   = "NCL-BYR-03"	#-- set the colormap to be used
+res.cnFillMode		= "RasterFill"
+res.cnLineLabelsOn  = False			#-- turn off contour line labels
+res.cnLinesOn		= False			#-- turn off contour lines
+res.cnInfoLabelOn	= False			#-- turn off contour info labels
+
+res.cnLevelSelectionMode = "ManualLevels" #-- select manual levels
+res.cnMinLevelValF	= -20.0 #-- minimum contour value
+res.cnMaxLevelValF	= +20.0 #-- maximum contour value
+res.cnLevelSpacingF	= 1.0	#-- contour increment
+
+res.mpGridAndLimbOn	= False
+res.mpOceanFillColor    = "Transparent"
+
+res.lbBoxLinesOn	= False
+res.lbLabelStride	= 10	#-- skip every other label
+#res.pmLabelBarOrthogonalPosF = -0.26 #-- move label upward (how much?)
+res.lbLabelFontHeightF	= 0.009		#-- label bar font height
+res.lbBoxMinorExtentF	= 0.24		#-- decrease height of labelbar box
+res.lbOrientation   = "horizontal"	#-- horizontal labelbar
+
+# res.tiMainString	= "Colors: temperature, Lines: ... "	#-- title string
+
 res.sfXArray        = lon
 res.sfYArray        = lat
 wks = ngl.open_wks(plottype,plotname)
@@ -111,7 +139,7 @@ if(os != None):
     # === what should we do with the offset? === #
 
 #-- Set the plot name
-plotname = f.variables['skt'].attributes['long_name'] + 'by Pedram, for Dear Saeed.'
+plotname = f.variables['skt'].attributes['long_name'] + ' -PS- index ' + str(check_index) + '-'
 
 #-- create the contour plot
 plot = ngl.contour_map(wks,var,res)
@@ -119,7 +147,7 @@ plot = ngl.contour_map(wks,var,res)
 #-- write variable long name and units to the plot
 txres   = ngl.Resources()
 txres.txFontHeightF = 0.012
-ngl.text_ndc(wks, plotname + 'In degrees Celsius. Date: ' + str(gettime(t_f)) + " vs " + str(gettime(t_i)) , 0.50,0.82,txres)
+ngl.text_ndc(wks, plotname + '(C). Date: ' + str(gettime(t_f)) + " vs " + str(gettime(t_i)), 0.50,0.82,txres)
 
 # Hard coded this because of inconsistency in data structure: f.variables['skt'].attributes['units']
 ngl.frame(wks)
